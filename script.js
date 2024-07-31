e5527e29a4990468fd68d5b4a043f3db3de7aa13
BellaAkins <sarabellaakins@gmail.com>
2024-07-31 00:40:13 -0700

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const close = document.querySelector(".close");
 const nav = document.querySelector(".navigation");
 
 menu.addEventListener("click", () => {
-  nav.style.width = "50%";
+  nav.style.width = "100%";
   nav.style.display = "block";
 });
 
